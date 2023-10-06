f0f1be6e57c3d2b0a1b3152d481f7771ab22fef5
Ollie Percivall <olliepercivall@hotmail.com>
2023-10-06 18:16:13 +0100

    textual changes

--- static/js/intro.js
+++ static/js/intro.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = "Welcome to Stage 1. The offices of VIPERCALL TECHNOLOGIES - YOUR FIRST DAY. \n\nYou are a keen gamer with decent technology skills and last week you finished working your dead-end job as a fast-food delivery rider. \n\nYou have just landed a job at a technology company who investigates online scams. \n\nThere is something new on the market - a groundbreaking and eagerly awaited video game called Peril Quest, \n\npowered by the first neural network games engine where players can enter breathtakingly realistic realms.\n\nAnd it's within this game that some of the most prolific online scams are currently taking place....";
+var txt = "Welcome to the offices of VIPERCALL TECHNOLOGIES - Your first day. \n\nYou are a keen gamer with decent technology skills and last week you finished working your dead-end job as a fast-food delivery rider. \n\nYou have just landed a job at a technology company who investigates scams within online games. \n\nThere is something new on the market - a groundbreaking and eagerly awaited video game called Peril Quest, \n\npowered by the first neural network games engine where players can enter breathtakingly realistic realms.\n\nAnd it's within this game that some of the most prolific online scams are currently taking place....";
 var speed = 40;
 
 function typeWriter() {
